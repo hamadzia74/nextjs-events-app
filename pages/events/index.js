9e80b1e56f11128ce533d1da48e0db89382807f1
HAMAD ZIA <hamadzia74@gmail.com>
2023-09-23 22:04:19 +0500

    Navigate to the "Filtered Events" Page Prgrammatically

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,12 +2,18 @@ import { Fragment } from "react";
 import EventList from "../../components/events/event-list";
 import { getAllEvents } from "../../dummy-data"
 import EventsSearch from "./events-search";
+import { useRouter } from "next/router";
 
 export default function AllEventPage() {
+  const router = useRouter();
   const events = getAllEvents();
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath);
+  }
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler}/>
       <EventList items={events} />
     </Fragment>
   )
