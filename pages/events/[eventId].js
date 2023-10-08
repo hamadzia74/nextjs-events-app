23e0f6abf9b4aafb6ee01b25885466d7a068013f
HAMAD ZIA <hamadzia74@gmail.com>
2023-10-08 17:14:51 +0500

    Optimized Data Fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router"
 // import { getEventById } from "../../dummy-data";
-import { getEventById, getAllEvents } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from '../../components/event-detail/event-summary'
 import EventLogistics from '../../components/event-detail/event-logistics'
 import EventContent from '../../components/event-detail/event-content'
@@ -14,7 +14,7 @@ export default function EventDetailPage(props) {
   const event = props.selectedEvent;
 
   if (!event) {
-    return <ErrorAlert><p>No event found!</p></ErrorAlert>
+    return <div className="center"><p>Loading...</p></div>
   }
 
   return (
@@ -34,14 +34,15 @@ export async function getStaticProps(context) {
   return {
     props: {
       selectedEvent: event
-    }
+    },
+    revalidate: 30
   }
 }
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map(event => ({ params: { eventId: event.id } }));
   return {
     paths: paths,
-    fallback: false
+    fallback: 'blocking'
   }
 }
