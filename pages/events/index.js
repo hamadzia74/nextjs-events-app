d4697522593b9709d15bd2c2eab3d9519fee5e04
HAMAD ZIA <hamadzia74@gmail.com>
2023-10-08 17:20:11 +0500

    Optimized All Events Page to get the updated data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,20 +1,31 @@
 import { Fragment } from "react";
 import EventList from "../../components/events/event-list";
-import { getAllEvents } from "../../dummy-data"
+// import { getAllEvents } from "../../dummy-data"
+import { getAllEvents } from "../../helpers/api-util"
 import EventsSearch from "./events-search";
 import { useRouter } from "next/router";
 
-export default function AllEventPage() {
+export default function AllEventPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  // const events = getAllEvents();
+  const { events } = props;
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`
     router.push(fullPath);
   }
   return (
     <Fragment>
-      <EventsSearch onSearch={findEventsHandler}/>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   )
+}
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events: events
+    },
+    revalidate: 60
+  };
 }
